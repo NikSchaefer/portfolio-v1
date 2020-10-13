3e0a193968231ef9693d28d031ae6dae404ac813
Tim Schaefer <nikkschaefer@gmail.com>
2020-10-13 13:21:56 -0500

    Card + Home rework

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,7 +2,6 @@ import React from 'react';
 import TEC from './Images/TEC.png'
 import Nebula from './Images/nebula.png'
 import k5red from './Images/k5red.png'
-import RB from './Images/ROCKETBEAST_PFP1.png'
 import { NavLink } from 'react-router-dom';
 
 import { animateScroll as scroll } from "react-scroll";
@@ -18,8 +17,7 @@ const Home = () => {
                 <p className='home-top-description-title'>Hi there, I'm Nik.</p>
                 <p className="home-top-description">
                     I am a self-taught front-end developer and a student located in Minnesota, US. 
-                    I am currently interning for <a className="home-top-description-link" href='https://www.theesportcompany.com/'>The Esport Company</a> and working 
-                    for <a className="home-top-description-link" href='https://nebulaesports.vercel.app/'>Nebula Esports</a>. 
+                    I am currently interning for <a className="home-top-description-link" href='https://www.theesportcompany.com/'>The Esport Company</a>.
 
                      <br /><br />
                     Feel free to send me a message for possible collaborations, 
@@ -39,52 +37,46 @@ const Home = () => {
             <div class="card" >
                 <img src={TEC} className="card-img" alt="" />
                 <div class="container">
-                    <h4><b>The Esport Company</b></h4>
+                    <p className='gray'>WEB DESIGN / UX / DEVELOPMENT</p>
+                    <h4 style={{fontSize:'30px'}}><b>The Esport Company</b></h4>
                     <p>Revamping the current website to fit to a growing buisness<br /></p>
-                </div>
-                    <p className="home-card-label-div-text-purple">
+                    <p className="home-card-label-div-text-blue" style={{backgroundColor:'#ccccfa'}}>
                         Internship
                     </p>
-
-            </div>
-
-            <div class="card">
-                <img src={k5red} alt="" className="card-img" />
-                <div class="container">
-                    <h4><b>K5 Gaming</b></h4>
-                    <p>Creating a new Website with React to fit a growing orginization</p>
                 </div>
-                <p className="home-card-label-div-text-blue">
-                        Full Stack + Design
-                    </p>
+                    
+
             </div>
 
             <div class="card">
                 <img src={Nebula} alt="" className="card-img" />
                 <div class="container">
+                    <p className='gray'>WEB DESIGN / MANAGEMENT / DEVELOPMENT</p>
                     <h4><b>Nebula Esports</b></h4>
                     <p>Full stack + Design working as a Project Lead</p>
-                </div>
-                <p className="home-card-label-div-text-orange">
-                Full Stack + Design
+                    <p className="home-card-label-div-text-blue" style={{backgroundColor:'#fed674'}}>
+                        Development + Design
                     </p>
+                </div>
+                
+            </div>
+            
+            <div class="card">
+                <img src={k5red} alt="" className="card-img" />
+                <div class="container">
+                    <p className='gray'>DESIGN / RESEARCH / DEVELOPMENT</p>
+                    <h4><b>K5 Gaming</b></h4>
+                    <p>Creating a new Website with React to fit a growing orginization</p>
+                    <p className="home-card-label-div-text-blue" style={{backgroundColor:'#b1e3ff'}}>
+                    Development + Design
+                    </p>
+                </div>
+                
             </div>
 
-            <div class="card">
-                <img src={RB} alt="" className="card-img-blur" />
-                <div class="container">
-                    <h4><b>RocketBeast</b></h4>
-                    <p>Coming Soon
-                    <br />
-                    <br />
-                    </p>
-                </div>
-                <p className="home-card-label-div-text-red">
-                        Full Stack + Design
-                    </p>
-                    
-                    
-            </div>
+           
+
+           
             </div>
             <div className="home-div-about">
             
