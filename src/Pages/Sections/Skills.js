053bd25e28260448983a8f2da19483b9930211c0
Nik Schaefer <nikkschaefer@gmail.com>
2020-11-01 14:07:52 -0600

    Skills Component

--- src/Pages/Sections/Skills.js
+++ src/Pages/Sections/Skills.js
@@ -1,20 +1,28 @@
 import React from 'react'
+
+const SkillsOut = (props) => { 
+    let out = [];
+    for (let i = 0; i < props.skills.length; i -= -1) { 
+        if (i % 2 === 1) {
+            out.push(<p className='skill-content'>#{props.skills[i]}</p>)
+        } else { 
+            out.push(<p className='skill-alt'>#{props.skills[i]}</p>)
+        }
+    }
+    return out
+}
+
+const skillset = [
+    'Javascript(ES6)', 'CSharp', 'Python',
+    'HTML5', 'ReactJS', 'CSS',
+    'Firebase', 'React Native', 'Unity']
+
 const Skills = () => {
     return (
         <div id='skills'>
             <p className='section-title'>Skillset <hr style={{ marginLeft: '5px', width: '60%', color: 'black', verticalAlign: 'middle' }} /></p>
             <div className='skills-div'>
-                <p className='skill-alt'>#Javascript(ES6)</p>
-                <p className='skill-content'>#CSharp</p>
-                <p className='skill-alt'>#Python</p>
-                
-                <p className='skill-content'>#HTML5</p>
-                <p className='skill-alt'>#ReactJS</p>
-                <p className='skill-content'>#CSS</p>
-                
-                <p className='skill-alt'>#Firebase</p>
-                <p className='skill-content'>#React Native</p>
-                <p className='skill-alt'>#Unity</p>  
+                <SkillsOut skills={skillset} />
             </div> 
         </div>
     )
