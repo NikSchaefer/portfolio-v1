532e6f2183528b5cd30f454a38105ca5a0be66ae
Nik Schaefer <nikkschaefer@gmail.com>
2020-11-23 10:49:29 -0600

    Intro Arrow

--- src/Pages/Sections/Intro.js
+++ src/Pages/Sections/Intro.js
@@ -1,17 +1,16 @@
 import React from 'react'
 import { toAbout } from './NavBar'
+import arrow from '../Images/arrow.svg'
 const Intro = () => {
     return (
-        <div>
             <div className="intro-div">
                     <p className='intro-text'>
                         <span role='img' aria-label='wave'>👋</span>I'm <span className='intro-name'>Nik Schaefer.</span><br />
                         I'm a student with a passion for <br />
                         Programming and learning
                     </p>
-                    <p className='intro-button' onClick={toAbout} id='intro-button-full'>Learn More</p>
+                    <p className='intro-button' onClick={toAbout} id='intro-button-full'>Learn More<img src={arrow} alt='arrow' className='intro-arrow' /></p>
                 </div>
-        </div>
     )
 }
 export default Intro;
